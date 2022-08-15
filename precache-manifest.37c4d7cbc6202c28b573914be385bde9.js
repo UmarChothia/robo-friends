2bf2e91f80a97c7766220517ee4cb3100fea045c
Umar <umarchothia@hotmail.co.uk>
2022-08-15 21:59:40 +0100

    Updates

---
+++ precache-manifest.37c4d7cbc6202c28b573914be385bde9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "614fd119725f934eb16d",
+    "url": "/robo-friends/static/css/main.fae604a3.chunk.css"
+  },
+  {
+    "revision": "614fd119725f934eb16d",
+    "url": "/robo-friends/static/js/main.b4edeb88.chunk.js"
+  },
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "82690006ea868802418c",
+    "url": "/robo-friends/static/css/2.7c22a25b.chunk.css"
+  },
+  {
+    "revision": "82690006ea868802418c",
+    "url": "/robo-friends/static/js/2.f2c8162a.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3e2a05fa8ea19f282fef6e4e9a7ed802",
+    "url": "/robo-friends/index.html"
+  }
+];
